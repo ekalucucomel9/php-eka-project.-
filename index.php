b98d8e7fc28514f26e53f5e12e420aec206c2517
eka-lestari <ekhaaaaa1509@gmail.com>
2026-01-07 13:08:14 +0700

    commit : add file server validation for form inputs

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar API PHP</title>
+</head>
+<body>
+    <h1>Message form</h1>
+    <form action="submit.php" method="POST">
+        <label>Nama :</label>
+        <input type="text" name="nama"> <br>
+
+        <label>Pesan :</label>
+        <textarea name="pesan"></textarea> <br>
+
+        <button type="submit">submit</button>
+    </form>   
+</body>
+</html>
